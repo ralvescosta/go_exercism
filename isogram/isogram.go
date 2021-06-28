e4a9a067cd1a2f7daaee590573b81b93651f217b
ralvescosta <rafael.rac.mg@gmail.com>
2021-06-28 05:56:53 -0300

    refactor: isogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,1 +1,25 @@
 package isogram
+
+import "strings"
+
+func IsIsogram(word string) bool {
+	if word == "" {
+		return true
+	}
+
+	letters := make(map[string]int)
+	trim := strings.ReplaceAll(word, " ", "")
+	replace := strings.ReplaceAll(trim, "-", "")
+	lower := strings.ToLower(replace)
+	isIsogram := true
+
+	for _, c := range lower {
+		if letters[string(c)] == 1 {
+			isIsogram = false
+			break
+		}
+		letters[string(c)] += 1
+	}
+
+	return isIsogram
+}
