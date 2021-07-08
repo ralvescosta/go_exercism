4299b1da95d30f8ea295bee01d26b044a434117e
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-07 21:17:48 -0300

    feat: robot-name

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,9 +1,33 @@
 package robotname
 
-type Robot struct{}
+import (
+	"fmt"
+	"math/rand"
+)
 
-func (Robot) Name() (string, error) {
-	return "", nil
+type Robot struct {
+	RobotName string
 }
 
-func (Robot) Reset() {}
+var letters = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randomString() string {
+	return string(letters[rand.Intn(26)])
+}
+
+func (r *Robot) Name() (string, error) {
+	if r.RobotName == "" {
+		r1 := rand.Intn(9)
+		r2 := rand.Intn(9)
+		r3 := rand.Intn(9)
+		s1 := randomString()
+		s2 := randomString()
+		r.RobotName = fmt.Sprintf("%s%s%d%d%d", s1, s2, r1, r2, r3)
+	}
+
+	return r.RobotName, nil
+}
+
+func (r *Robot) Reset() {
+	r.RobotName = ""
+}
