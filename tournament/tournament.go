73d8c8fbdb5e51d045a2055f61be6de7760d41dc
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-09 21:01:08 -0300

    refactor: tournament

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -3,6 +3,7 @@ package tournament
 import (
 	"errors"
 	"io"
+	"sort"
 	"strings"
 )
 
@@ -71,50 +72,55 @@ type Match struct {
 }
 
 type Score struct {
-	MP int
-	W  int
-	D  int
-	L  int
-	P  int
+	Team string
+	MP   int
+	W    int
+	D    int
+	L    int
+	P    int
 }
 
 type ScoreBoard struct {
-	Board map[string]Score
+	Board []Score
 }
 
 func (sb *ScoreBoard) initializeScoreboard(m Match) {
 	if m.Winier == "" {
-		sb.Board = map[string]Score{
-			m.Home: {
-				MP: 1,
-				W:  0,
-				D:  1,
-				L:  0,
-				P:  1,
+		sb.Board = []Score{
+			{
+				Team: m.Home,
+				MP:   1,
+				W:    0,
+				D:    1,
+				L:    0,
+				P:    1,
 			},
-			m.Visiting: {
-				MP: 1,
-				W:  0,
-				D:  1,
-				L:  0,
-				P:  1,
+			{
+				Team: m.Visiting,
+				MP:   1,
+				W:    0,
+				D:    1,
+				L:    0,
+				P:    1,
 			},
 		}
 	} else {
-		sb.Board = map[string]Score{
-			m.Winier: {
-				MP: 1,
-				W:  1,
-				D:  0,
-				L:  0,
-				P:  3,
+		sb.Board = []Score{
+			{
+				Team: m.Home,
+				MP:   1,
+				W:    1,
+				D:    0,
+				L:    0,
+				P:    3,
 			},
-			m.Loser: {
-				MP: 1,
-				W:  0,
-				D:  0,
-				L:  1,
-				P:  0,
+			{
+				Team: m.Visiting,
+				MP:   1,
+				W:    0,
+				D:    0,
+				L:    1,
+				P:    0,
 			},
 		}
 	}
@@ -124,6 +130,99 @@ func (sb *ScoreBoard) UpdateBoard(match Match) {
 	if sb.Board == nil {
 		sb.initializeScoreboard(match)
 		return
+	}
+
+	if match.Winier != "" {
+		indexWinier := sort.Search(len(sb.Board), func(i int) bool {
+			return sb.Board[i].Team == match.Winier
+		})
+		indexLoser := sort.Search(len(sb.Board), func(i int) bool {
+			return sb.Board[i].Team == match.Loser
+		})
+
+		if indexWinier < len(sb.Board) {
+			sb.Board[indexWinier].W += 1
+			sb.Board[indexWinier].P += 3
+			sb.Board[indexWinier].MP += 1
+
+			if indexWinier > 1 && sb.Board[indexWinier].P >= sb.Board[indexWinier-1].P {
+				old := sb.Board[indexWinier-1]
+				sb.Board[indexWinier-1] = sb.Board[indexWinier]
+				sb.Board[indexWinier] = old
+			}
+		} else {
+			sb.Board = append(sb.Board, Score{
+				Team: match.Winier,
+				MP:   1,
+				W:    1,
+				D:    0,
+				L:    0,
+				P:    3,
+			})
+		}
+
+		if indexLoser < len(sb.Board) {
+			sb.Board[indexLoser].L += 1
+			sb.Board[indexLoser].MP += 1
+		} else {
+			sb.Board = append(sb.Board, Score{
+				Team: match.Loser,
+				MP:   1,
+				W:    0,
+				D:    0,
+				L:    1,
+				P:    0,
+			})
+		}
+	} else {
+		homeIndex := sort.Search(len(sb.Board), func(i int) bool {
+			return sb.Board[i].Team == match.Home
+		})
+		visitingIndex := sort.Search(len(sb.Board), func(i int) bool {
+			return sb.Board[i].Team == match.Visiting
+		})
+
+		if homeIndex < len(sb.Board) {
+			sb.Board[homeIndex].W += 1
+			sb.Board[homeIndex].P += 3
+			sb.Board[homeIndex].MP += 1
+
+			if homeIndex > 1 && sb.Board[homeIndex].P >= sb.Board[homeIndex-1].P {
+				old := sb.Board[homeIndex-1]
+				sb.Board[homeIndex-1] = sb.Board[homeIndex]
+				sb.Board[homeIndex] = old
+			}
+		} else {
+			sb.Board = append(sb.Board, Score{
+				Team: match.Home,
+				MP:   1,
+				W:    0,
+				D:    1,
+				L:    0,
+				P:    1,
+			})
+		}
+
+		if visitingIndex < len(sb.Board) {
+			sb.Board[visitingIndex].W += 1
+			sb.Board[visitingIndex].P += 3
+			sb.Board[visitingIndex].MP += 1
+
+			if visitingIndex > 1 && sb.Board[visitingIndex].P >= sb.Board[visitingIndex-1].P {
+				old := sb.Board[visitingIndex-1]
+				sb.Board[visitingIndex-1] = sb.Board[visitingIndex]
+				sb.Board[visitingIndex] = old
+			}
+		} else {
+			sb.Board = append(sb.Board, Score{
+				Team: match.Visiting,
+				MP:   1,
+				W:    0,
+				D:    1,
+				L:    0,
+				P:    1,
+			})
+		}
 	}
 }
 
