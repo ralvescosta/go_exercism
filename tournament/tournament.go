9cf664b23e65d5a3e2c37fbfbd9da9c518f9a2ea
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-16 07:00:05 -0300

    refactor: tournament

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -11,48 +11,10 @@ import (
 
 func Tally(reader io.Reader, writer io.Writer) error {
 	rows := read(reader)
-	hashMap := make(map[string]*TeamScore)
-	var board Scoreboard
-
-	for i, row := range rows {
-		if len(strings.Trim(row, " ")) == 0 || row[0] == '#' {
-			continue
-		}
-		match, err := parseMatch(row)
-		if err != nil {
-			return err
-		}
-		if _, ok := hashMap[match.HomeTeam]; !ok {
-			hashMap[match.HomeTeam] = &TeamScore{match.HomeTeam, 0, 0, 0, 0, 0}
-			board = append(board, hashMap[match.HomeTeam])
-		}
-		if _, ok := hashMap[match.VisitingTeam]; !ok {
-			hashMap[match.VisitingTeam] = &TeamScore{match.VisitingTeam, 0, 0, 0, 0, 0}
-			board = append(board, hashMap[match.VisitingTeam])
-		}
-		team1, team2 := hashMap[match.HomeTeam], hashMap[match.VisitingTeam]
-		team1.MatchesPlayed += 1
-		team2.MatchesPlayed += 1
-		switch match.Result {
-		case "win":
-			team1.Wins += 1
-			team1.Points += 3
-			team2.Losses += 1
-		case "loss":
-			team1.Losses += 1
-			team2.Wins += 1
-			team2.Points += 3
-		case "draw":
-			team1.Draws += 1
-			team2.Draws += 1
-			team1.Points += 1
-			team2.Points += 1
-		default:
-			return fmt.Errorf("invalid match result \"%s\" in line %d", match.Result, i)
-		}
+	board, err := process(rows)
+	if err != nil {
+		return err
 	}
-
-	sort.Sort(board)
 	writer.Write([]byte("Team                           | MP |  W |  D |  L |  P\n"))
 	for _, team := range board {
 		fmt.Fprintf(writer, "%-31s| %2d | %2d | %2d | %2d | %2d\n",
@@ -63,7 +25,6 @@ func Tally(reader io.Reader, writer io.Writer) error {
 			team.Losses,
 			team.Points)
 	}
-
 	return nil
 }
 
@@ -115,4 +76,48 @@ func parseMatch(match string) (Match, error) {
 		VisitingTeam: record[1],
 		Result:       record[2],
 	}, nil
+}
+
+func process(rows []string) (Scoreboard, error) {
+	hashMap := make(map[string]*TeamScore)
+	var board Scoreboard
+	for i, row := range rows {
+		if len(strings.Trim(row, " ")) == 0 || row[0] == '#' {
+			continue
+		}
+		match, err := parseMatch(row)
+		if err != nil {
+			return Scoreboard{}, err
+		}
+		if _, ok := hashMap[match.HomeTeam]; !ok {
+			hashMap[match.HomeTeam] = &TeamScore{match.HomeTeam, 0, 0, 0, 0, 0}
+			board = append(board, hashMap[match.HomeTeam])
+		}
+		if _, ok := hashMap[match.VisitingTeam]; !ok {
+			hashMap[match.VisitingTeam] = &TeamScore{match.VisitingTeam, 0, 0, 0, 0, 0}
+			board = append(board, hashMap[match.VisitingTeam])
+		}
+		team1, team2 := hashMap[match.HomeTeam], hashMap[match.VisitingTeam]
+		team1.MatchesPlayed += 1
+		team2.MatchesPlayed += 1
+		switch match.Result {
+		case "win":
+			team1.Wins += 1
+			team1.Points += 3
+			team2.Losses += 1
+		case "loss":
+			team1.Losses += 1
+			team2.Wins += 1
+			team2.Points += 3
+		case "draw":
+			team1.Draws += 1
+			team2.Draws += 1
+			team1.Points += 1
+			team2.Points += 1
+		default:
+			return Scoreboard{}, fmt.Errorf("invalid match result \"%s\" in line %d", match.Result, i)
+		}
+	}
+	sort.Sort(board)
+	return board, nil
 }
